b4d244bf99734aaac7df421b0e5b36f0d3676ff6
denisskachko <denix.sg@gmail.com>
2015-10-10 23:51:12 +0300

    fix http

---
+++ social/GlobalWidget.js
@@ -0,0 +1,177 @@
+﻿window.zs = (function (zs) {
+    zs.namespace = function () {
+        var a = arguments, o = null, i, j, d;
+        for (i = 0; i < a.length; i = i + 1) {
+            d = a[i].split(".");
+            o = window;
+            for (j = 0; j < d.length; j = j + 1) {
+                o[d[j]] = o[d[j]] || {};
+                o = o[d[j]];
+            }
+        }
+        return o;
+    };
+
+    String.format = function () {
+        var s = arguments[0];
+        for (var i = 0; i < arguments.length - 1; i++) {
+            var reg = new RegExp("\\{" + i + "\\}", "gm");
+            s = s.replace(reg, arguments[i + 1]);
+        }
+
+        return s;
+    };
+
+    zs.settings = {
+        offset: 100,
+        clientWidthDlg: function () {
+            var width = Math.round(window.zs.jQuery(document).width() / 1.5);
+            return width;
+        },
+
+        clientHeightDlg: function () {
+            var height = Math.round(window.zs.jQuery(document).height() / 1.2);
+            return height;
+        },
+        confirmationOKConfig:
+            {
+                "OK": function () {
+                    window.zs.jQuery(this).dialog("close");
+                }
+            },
+        effect: 'slide'
+        //The effect to be used. Possible values: 'blind', 'clip', 'drop', 'explode', 'fold', 'puff', 'slide', 'scale', 'size', 'pulsate'.
+    };
+
+    String.prototype.trunc =
+        function (n, useWordBoundary) {
+            var toLong = this.length > n,
+                s_ = toLong ? this.substr(0, n - 1) : this;
+            s_ = useWordBoundary && toLong ? s_.substr(0, s_.lastIndexOf(' ')) : s_;
+            return toLong ? s_ + '...' : s_;
+        };
+
+    zs.GetValueById = function (elementId) {
+        return window.zs.jQuery('#' + elementId).val();
+    };
+
+    zs.GetValueByName = function (elementName) {
+        return window.zs.jQuery('[name=' + elementName + ']').val();
+    };
+
+    zs.infoErrorMsg = function (text, container, hide){
+        return this.informationMessage(text, 'alert', false, container, hide);
+    };
+
+    zs.infoSuccessMsg = function (text, container, hide){
+        return this.informationMessage(text, 'success', false, container, hide);
+    };
+
+	zs.informationMessage = function(text, classType, isAppend, container, hide) {
+		if (!container) {
+			if ($('#message').length === 0) {
+				$('<div id=\'message\'></div>').appendTo('#containerContent');
+			}
+			var c = $('#message');
+			container = c.fadeIn();
+		} else {
+			container.fadeIn(3000);
+			container.empty();
+		}
+
+		if (hide) {
+			container.fadeOut(3000);
+			return;
+		}
+
+		isAppend = isAppend || false;
+		var result;
+		if (isAppend) {
+			result = container.append('<div class="alert-box ' + classType + ' ]">' + text + '<a href="" class="close">&times;</a></div>');
+		} else {
+			result = container.html('<div class="alert-box ' + classType + ' ]">' + text + '<a href="" class="close">&times;</a></div>');
+		}
+		return result;
+	};
+
+    function InfoDialog(selectorValue, url, height, width, btnConfig, loadingContainer, attachId, titleText) {
+        window.zs.jQuery(function () {
+            window.zs.jQuery(selectorValue).each(function (i, element) {
+                var id = window.zs.jQuery(element).attr("id");
+                window.zs.jQuery(element).click(function (event) {
+                    event.preventDefault();
+
+                    var containerDiv = window.zs.jQuery("#infoDialog");
+                    if (!containerDiv || containerDiv.length == 0) {
+                        containerDiv = window.zs.jQuery('<div id="infoDialog"></div>').appendTo(document.body);
+                    }
+                    else {
+                        containerDiv.empty();
+                    }
+
+                    window.zs.jQuery(containerDiv).dialog({
+                        autoOpen: false,
+                        draggable: true,
+                        width: width || zs.settings.clientWidthDlg(),
+                        height: height || zs.settings.clientHeightDlg(),
+                        resizable: false,
+                        title: titleText || 'Information',
+                        modal: true,
+                        open: function (event, ui) {
+                            var addId = attachId || true;
+                            var resultUrl = attachId ? (url + id) : url;
+                            window.zs.jQuery(containerDiv).load(resultUrl);
+                        },
+                        buttons: btnConfig || zs.settings.confirmationOKConfig
+                    });
+
+                    window.zs.jQuery(containerDiv).dialog('open');
+
+                    window.zs.jQuery(containerDiv).bind("keypress", function (e) {
+                        if (e.keyCode == 13) {
+                            return false;
+                        }
+                        return true;
+                    });
+                });
+            });
+        });
+    }
+
+	// todo : remove it if there is no use
+    function EmebedDialog(selectorValue, url, containerSelector, attachId, cancelSelector) {
+    	window.zs.jQuery(document).ready(function () {
+    		var effect = zs.settings.effect;
+    		var html = window.zs.jQuery(containerSelector).html();
+
+    		window.zs.jQuery(cancelSelector).live('click', function () {
+    			window.zs.jQuery(containerSelector).html(html);
+    			window.zs.jQuery(containerSelector).show(effect);
+    		});
+
+    		window.zs.jQuery(selectorValue).live('click', function (event) {
+    			event.preventDefault();
+    			var element = window.zs.jQuery(event.currentTarget);
+    			var id = window.zs.jQuery(element).attr('id');
+    			var containerDiv = window.zs.jQuery(containerSelector);
+    			var resultUrl = attachId ? (url + '/' + id) : url;
+
+    			containerDiv.hide(effect);
+    			containerDiv.load(resultUrl);
+    			containerDiv.show(effect);
+
+    			window.zs.jQuery(containerDiv).bind("keypress", function (e) {
+    				if (e.keyCode == 13) {
+    					return false;
+    				}
+    				return true;
+    			});
+    		});
+    	});
+
+    }
+
+    return zs;
+})(window.zs || {});
+
+
---
+++ social/widgetController.js
@@ -0,0 +1,257 @@
+﻿//window.zs.step1
+;window.zs.step1 = (function step1($, step1, undefined) {
+    var o = step1 || {};
+    o.LoadServices = function (providerId) {
+        window.zs.UpdateStep(0);
+        window.zs.Step1.LoadServices(providerId);
+    };
+    o.UpdateStep = function (stepNumber) {
+        $('.zr-steps').show();
+        $('.zr-steps li.zr-active').removeClass('zr-active');
+        $($('.zr-steps li').get(stepNumber)).addClass('zr-active');
+    };
+    
+    o.HideStep = function (){
+        $('.zr-steps').hide();
+    };
+
+    return o;
+})(window.zs.jQuery || $, window.zs.step1 || {});
+
+//window.zs.step1
+ window.zs.step2 = (function step2($, step2, undefined) {
+    var calendarSelector = "a[date]";
+    var bookingLinkSelector = "#book";
+    var selectedInfo = "#selectedInfo";
+    var serviceName = "#serviceName";
+    var resourceSelector = "a[rkey]";
+    var timeSelector = "a[time]";
+    var calendarContainerSelector = '._DayToBook';
+    var serviceId = 'input[name="serviceId"]';
+    var providerId = 'input[name="providerId"]';
+    var serviceType = 'input[name="serviceType"]';
+    var resourceId = 'input[name="resourceId"]';
+    var selectedResourceId = '.selected>a[rkey]';
+    var o = step2 || {};
+    o.selectedTime = null;
+    o.dayId = null;
+    o.ticks = null;
+    o.direction = null;
+    o.resourceId = null;
+    o.providerId = null;
+    o.updateCalendar = function (e) {
+        var link = $(e.currentTarget);
+        $(calendarSelector).parent('li').removeClass('selected');
+        $(link).parent('li').addClass('selected');
+        o.dayId = $(link).attr('date');
+        o.ticks = $(link).attr('ticks');
+        o.resourceId = $(selectedResourceId).attr('rkey');
+        o.providerId = $(providerId).val();
+        zs.GetDayToBookByDayId(calendarContainerSelector, o.getParams());
+    };
+    o.updateResource = function (e) {
+        var link = $(e.currentTarget);
+        $(resourceSelector).parent('li').removeClass('selected');
+        $(link).parent('li').addClass('selected');
+        o.dayId = $('li[class="selected"] > a[date]').attr('date');
+        o.resourceId = $(link).attr('rkey');
+        o.providerId = $(providerId).val();;
+        zs.GetDayToBookByDayId(calendarContainerSelector, o.getParams());
+        o.updateTime();
+
+    };
+
+    o.setSelectedInfo = function (){
+//        var text = 'Выбрано: ';
+//        text += $('#serviceName').text();
+//        text += ', ' + $('li[class=selected]>a[rkey]').text();
+//        text +=', ' + $('li[class=selected]>a[date]').text();
+//        text += ', ' + $('li[class=selected]>a[time]').text();
+//
+//        $(selectedInfo).text(text);
+    };
+
+    o.checkSelection = function () {
+        o.resourceId = $('li[class=selected]>a[rkey]').attr('rkey');
+        o.selectedTime = $('li[class=selected]>a[time]').attr('time');
+        o.providerId = $(providerId).val();
+        o.serviceType = $(serviceType).val();
+
+        if (o.serviceType == 'BookingByDays') {
+            o.dayId = o.selectedTime;
+
+            if (o.dayId != null && o.resourceId != null && o.selectedTime != null) {
+                $(bookingLinkSelector).show();
+                o.setSelectedInfo();
+                $(selectedInfo).show();
+                return true;
+            }
+        } else {
+            o.dayId = $('li[class=selected]>a[date]').attr('date');
+            if (o.dayId != null && o.resourceId != null && o.selectedTime != null) {
+                $(bookingLinkSelector).show();
+                o.setSelectedInfo();
+                $(selectedInfo).show();
+                return true;
+            }
+        }
+        $(bookingLinkSelector).hide();
+        $(selectedInfo).hide();
+        return true;
+    };
+    o.getParams = function () {
+        var res = {};
+        for (var key in o) {
+            if (o.hasOwnProperty(key) && (!$.isFunction(o[key]))) {
+                res[key] = o[key];
+            }
+        }
+        res.serviceId = $(serviceId).val();;
+        return (res);
+    };
+    o.sendSelection = function () {
+        if (o.checkSelection()) {
+            var p = {stepNumber:3, serviceId: $(serviceId).val(), resourceId: o.resourceId, providerId: o.providerId, selectedTime: o.selectedTime, dayId: o.dayId, useMaster:true };
+            window.zs.LoadStep(p);
+        }
+    };
+    o.updateScrollCalendar = function (e) {
+        var link = $(e.currentTarget);
+        var scrollCalendarContainer = $('._ScrollCalendar');
+        var p = {};
+        p.direction = 1;
+        if ($(link).attr('data') === "prev") {
+            p.direction = 0;
+        }
+        p.ticks = $(link).attr("ticks");
+        zs.GetScrollCalendar(scrollCalendarContainer, p);
+    };
+    o.updateTime = function (e) {
+        if (e) {
+            var link = $(e.currentTarget);
+            if ($(link).parent('li').hasClass('zr-lock')) return;
+            $(timeSelector).parent('li').removeClass('selected');
+            $(link).parent('li').addClass('selected');
+            o.selectedTime = link.attr('time');
+        }
+        else {
+            $('.zr-time-select li').removeClass('selected');
+        }
+    };
+    o.bindClick = function (){
+        window.zs.step1.UpdateStep(1);
+        $('a', calendarSelector, resourceSelector, timeSelector, bookingLinkSelector).off('click');
+        $(calendarSelector).off('click');
+        $(resourceSelector).off('click');
+        $(timeSelector).off('click');
+        $(bookingLinkSelector).off('click');
+        $('a').off('click');
+        $('a[data="prev"],a[data="next"]').off('click');
+        $(calendarSelector).on('click', o.updateCalendar);
+        $(resourceSelector).on('click', o.updateResource);
+        $(timeSelector).on('click', o.updateTime);
+        $(bookingLinkSelector).one('click', o.sendSelection);
+        $('a').on('click', o.checkSelection);
+        $('a[data="prev"],a[data="next"]').on('click', o.updateScrollCalendar);
+    };
+
+    $(o.bindClick());
+    return o;
+})(window.zs.jQuery || $, window.zs.step2 || {});
+
+//window.zs.step3
+window.zs.step3 = (function step3($, step3, undefined) {
+    var o = step3 || {};
+    var zakazSel = o.zakazSel = '.zr-zakaz';
+    var clientNameSel = o.clientNameSel = 'input[wparam="Name"]';
+    var serviceId = o.serviceId = 'input[name="serviceId"]';
+    var providerId = o.providerId = 'input[name="providerId"]';
+    var sessionId = o.sessionId = 'input[name="sessionId"]';
+    var resourceId = o.resourceId = 'input[name="resourceId"]';
+    var startDateTicks = o.startDateTicks = 'input[name="startDateTicks"]';
+    var clientPhoneSel = o.clientPhoneSel = 'input[wparam="Phone"]';
+    var clientCommentSel = o.clientCommentSel = 'textarea[wparam="Comment"]';
+    var clientEmailSel = o.clientEmailSel = 'input[wparam="Email"]';
+    var clientRemindHoursSel = o.clientRemindHoursSel = 'select[wparam="RemindHours"]';
+    var clientEmailCheckBoxSel = o.clientEmailCheckBoxSel = 'input[wparam="IsEmailSpecified"]';
+    var phoneVerificationCodeSel = o.phoneVerificationCodeSel = 'input[wparam="PhoneVerificationCode"]';
+    var submitCodeButtonSel = o.submitCodeButtonSel = '.submitCodeButton';
+    var phoneMask = '+375-(99)-999-99-99';
+    function BuildParams() {
+        var p = {
+            serviceId: $(serviceId).val(),
+            providerId: $(providerId).val(),
+            resourceId: $(resourceId).val(),
+            startDateTicks: $(startDateTicks).val(),
+            clientName: $(clientNameSel).val(),
+            clientPhone: $(clientPhoneSel).val(),
+            comment: $(clientCommentSel).val(),
+            email: $(clientEmailSel).val(),
+            isEmailSpecified: $(clientEmailCheckBoxSel).is(":checked"),
+            remindHours: $(clientRemindHoursSel).val(),
+            sessionId: $(sessionId).val(),
+            phoneVerificationCode: $(phoneVerificationCodeSel).val()
+        };
+        
+        var result = {};
+        var $inputs = $('.zr-dict-fields :input');
+        $inputs.each(function () {
+            
+            var item = this;
+            if (result[item.name] != undefined && (result[item.name] == 'false' || result[item.name] == 'true')) {
+                result[item.name] = true; // it can be only checkbox.
+            } else {
+                result[item.name] = $(item).val();
+            }
+        });
+
+        $inputs = $('.zr-dict-fields :checkbox');
+        $inputs.each(function () {
+            var item = this;
+            result[item.name] = $(item).is(':checked');
+        });
+
+        p.CustomFieldValuesForRequestValues = JSON.stringify(result);
+
+        return p;
+    }
+    
+
+    o.IsEmailOn = function () {
+        if ($(clientEmailCheckBoxSel).is(":checked")) {
+            $(clientEmailSel).show();
+        }
+        else {
+            $(clientEmailSel).hide();
+        }
+        
+    };
+    o.sendRequest = function () {
+        window.zs.Step4Save(o.getParams());
+       
+    };
+    o.getParams = function () {
+        return BuildParams();
+    };
+
+    o.SendVerificationCode = function (){
+        $(submitCodeButtonSel).hide();
+        zs.widgetSuccessMsg('Проверочный код выслан на указанный номер. Пожалуйста, подождите ответа и введите код.');
+        $.when(window.zs.SendVerificationCode(o.getParams()))
+        .then(setTimeout(function () {
+            $(submitCodeButtonSel).show();
+        }, 15000));
+    };
+    o.bindClick = function () {
+        $(zakazSel).off('click').on('click', o.sendRequest);
+        $(clientEmailCheckBoxSel).off('click').on('click', o.IsEmailOn);
+        $(submitCodeButtonSel).off('click').on('click', o.SendVerificationCode);
+    };
+    o.init = function (){
+        $(zakazSel).show();
+//        $(clientPhoneSel).mask(phoneMask);
+        window.zs.step1.UpdateStep(2);
+        o.bindClick();
+    };
+    return o;
+})(window.zs.jQuery || $, window.zs.step3 || {});
